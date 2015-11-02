71e886a4361683610b43e9ffccf8d67d112b3a42
mhbeyatli <habibbeyatli@gmail.com>
2015-11-02 15:31:30 +0300

    habibten bişeyler

---
+++ players.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, id, name="", surname="", nationality="", style=""):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.nationality = nationality
+        self.style = style
