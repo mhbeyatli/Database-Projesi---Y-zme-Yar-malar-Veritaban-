4999ae0709f111528e1ee8728c0830844c1ff66d
mhbeyatli <beyatli@itu.edu.tr>
2015-11-19 18:57:35 +0200

    deleted Type.py1

--- Type.py
+++
@@ -1,7 +0,0 @@
-class types:
-    def __init__(self, id, name="", type="", weight="", height=""):
-        self.id = id
-        self.name = name
-        self.type = type
-        self.weight = weight
-        self.height = height
