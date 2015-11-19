af92c15728d5607aef1f13d1e73765150eb3c8a3
mhbeyatli <beyatli@itu.edu.tr>
2015-11-19 19:04:20 +0200

    Deleting other py files except server.py

--- contest.py
+++
@@ -1,7 +0,0 @@
-class contest:
-    def __init__(self, id, name="", surname="", type="", degree=""):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.type = type
-        self.degree =degree
--- players.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, id, name="", surname="", nationality="", style=""):
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.nationality = nationality
-        self.style = style
