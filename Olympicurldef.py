447c18c7d468eb93910f3e985b27b87431a26625
yoktan <yoktan@160.75.199.102>
2015-11-23 23:01:56 +0200

    changed in navigation bar

--- Olympicurldef.py
+++ Olympicurldef.py
@@ -1,11 +1,11 @@
-+import datetime
-+
-+from flask import render_template
-+
-+from config import app
-+
-+
-+@app.route('/Olympics')
-+def olympics_page():
-+    now = datetime.datetime.now()
-+    return render_template('Olympics.html', current_time=now.ctime())
+import datetime
+
+from flask import render_template
+
+from config import app
+
+
+@app.route('/Olympics')
+def olympics_page():
+    now = datetime.datetime.now()
+    return render_template('Olympics.html', current_time=now.ctime())
