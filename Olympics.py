7fd475a55367fea35a3c68954eb5511d098dd1aa
Anıl AĞCA <agcaan@itu.edu.tr>
2015-11-23 21:15:41 +0200

    Entity Olympics ,Add/Delete/Update/Search Operations Included

--- Olympics.py
+++ Olympics.py
@@ -1,11 +1,52 @@
-import datetime
-
+import psycopg2 as dbapi2
+from Olympics import Olympic
 from flask import render_template
+import datetime
+from flask import redirect
+from flask import url_for
 
-from config import app
 
+    def get_olympic(self, key):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "SELECT LISTNO,FULLNAME,SWIMMERID,YEAR,POOLID FROM OLYMPICS WHERE (LISTNO = %s)"
+            cursor.execute(query, (key))
+            Listno,Fullname,SwimmerId,Year,Poolid = cursor.fetchone()
+            return Olympic(Listno,Fullname,SwimmerId,Year,Poolid)
+    def get_olympics(self):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "SELECT * FROM OLYMPICS ORDER BY LISTNO"
+            cursor.execute(query)
+            Olympics = [Olympic(Listno,Fullname,SwimmerId,Year,Poolid))
+                      for Listno,Fullname,SwimmerId,Year,Poolid in cursor]
+            return Olympics
+	
+    def add_olympic(self, Olymp):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "INSERT INTO OLYMPICS (LISTNO, FULLNAME, SWIMMERID, YEAR, POOLID ) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(query, (Olymp.Listno, Olymp.Fullname, Olymp.SwimmerId,Olymp.Year, Olymp.Poolid))
+            connection.commit()
+            self.last_key = cursor.lastrowid
+    def delete_olympic(self, key):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "DELETE FROM OLYMPICS WHERE (LISTID = %s)"
+            cursor.execute(query, (key,))
+            connection.commit()
 
-@app.route('/Olympics')
-def olympics_page():
-    now = datetime.datetime.now()
-    return render_template('Olympics.html', current_time=now.ctime())
+    def update_olympic(self, Updateto, Olymp):
+        with dbapi2.connect(self.dsn) as connection:
+            cursor = connection.cursor()
+            query = "UPDATE OLYMPICS SET LISTNO = %s, FULLNAME = %s, SWIMMERID = %s, YEAR, POOLID WHERE (LISTNO = %s)"
+            cursor.execute(query, (Olymp.Listno, Olymp.Fullname, Olymp.SwimmerId,Olymp.Year, Olymp.Poolid, Updateto))
+            connection.commit()
+
+  def olympic_search(self,tosearch)
+	with dbapi2.connect(self.dsn) as connection:
+		cursor = connection.cursor()
+		query = "SELECT LISTNO,FULLNAME,SWIMMERID,YEAR,POOLID FROM OLYMPICS WHERE (FULLNAME LIKE %s)"
+		cursor.execute(query,tosearch)
+		Listno,Fullname,SwimmerId,Year,Poolid = cursor.fetchone()
+ 		return Olympic(Listno,Fullname,SwimmerId,Year,Poolid)
---
+++ Olympics_d.py
@@ -0,0 +1,61 @@
+import datetime
+from flask import redirect
+from flask import request
+from flask import url_for
+from flask import render_template
+from config import app
+
+class Olympic:
+    def __init__(self,Listno,Fullname,SwimmerId,Year,Poolid):
+            self.Listno = Listno
+            self.Fullname = Fullname
+		self.SwimmerId = SwimmerId
+		self.Year = Year
+		self.Poolid = Poolid
+@app.route('/Olympics', methods=['GET', 'POST'])
+def olympics_page():
+    if request.method == 'GET':
+        Olympics = app.store.get_olympics()
+        now = datetime.datetime.now()
+        return render_template('Olympics.html', Olympics=Olympics,
+                               current_time=now.ctime())
+    elif 'deleteolympics' in request.form:
+        keys = request.form.getlist('deleteolympics')
+        for key in keys:
+            app.store.delete_olympic(int(key))
+            return redirect(url_for('olympic_page'))
+
+    else:
+        Listno = request.form['Listno']
+        Fullname = request.form['Fullname']
+	SwimmerId = request.form['SwimmerId']
+	Year = request.form['Year']
+	Poolid = request.form['Poolid']
+        Olympic = Olympic(Listno,Fullname,SwimmerId,Year,Poolid)
+        app.store.add_olympic(Olympic)
+        return redirect(url_for('olympic_page', key=app.store.last_key))
+
+@app.route('/Olympics/<int:key>')
+def olympic_page(key):
+    Olympic= app.store.get_olympic(key)
+    now = datetime.datetime.now()
+    return render_template('Olympics.html', Olympic=Olympic, current_time=now.ctime())
+
+
+@app.route('/Olympics/update')
+def olympic_update():
+Toupdate = request.form['Toupdate']
+    Listno = request.form['Listno']
+        Fullname = request.form['Fullname']
+	SwimmerId = request.form['SwimmerId']
+	Year = request.form['Year']
+	Poolid = request.form['Poolid']
+        Olympic = Olympic(Listno,Fullname,SwimmerId,Year,Poolid)
+        app.store.Olympicupdate(Updateto,Olympic)
+        return redirect(url_for('olympic_page', key=app.store.last_key))
+
+@app.route('/Olympics/search')
+def olympic_search():
+	request.form['keywords']
+	app.store.olympicsearch(keywords)
+	return render_template('olympics_search.html', Olympic=Olympic, current_time=now.ctime())
