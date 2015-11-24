9a8414377211888b63a61b9d95c0d36170d15273
egecetindag <egecetindag@hotmail.com>
2015-11-24 15:15:13 +0200

    Update Styles.py
    Persons add delete functions have been added

--- Styles.py
+++ Styles.py
@@ -9,14 +9,18 @@ class Style:
     def __init__(self, title, year=None):
             self.title = title
             self.year = year
+class Person:
+    def __init__(self,cntry=None,time=None):
+            self.cntry= cntry
+            self.time= time
 
 @app.route('/Styles', methods=['GET', 'POST'])
 def styles_page():
     if request.method == 'GET':
         Styles = app.store.get_styles()
+        Persons = app.store.get_person()
         now = datetime.datetime.now()
-        return render_template('Styles.html', Styles=Styles,
-                               current_time=now.ctime())
+        return render_template('Styles.html', Styles=Styles, Persons=Persons, current_time=now.ctime())
     elif 'delete' in request.form:
         keys = request.form.getlist('styles_to_delete')
         for key in keys:
@@ -56,7 +60,6 @@ def style_update2():
     Styles = app.store.get_styles()
     now = datetime.datetime.now()
     return render_template('style_update.html',Styles=Styles,current_time=now.ctime())
-    
 @app.route('/Styles/search2')
 def style_search2():
     now = datetime.datetime.now()
@@ -69,3 +72,25 @@ def style_search():
         Styles=app.store.search_style(word)
         now = datetime.datetime.now()
         return render_template('Styles.html', Styles=Styles, current_time=now.ctime())
+
+@app.route('/Person', methods=['GET', 'POST'])
+def person_page():
+
+    if 'delete' in request.form:
+        keys = request.form.getlist('person_to_delete')
+        for key in keys:
+            app.store.delete_person(int(key))
+            return redirect(url_for('styles_page'))
+ 
+    else:
+        cntry = request.form['cntry']
+        time = request.form['time']
+        person1=Person(cntry,time)
+        app.store.add_person(person1)
+        return redirect(url_for('styles_page'))
+    
+@app.route('/Person/add')
+def person_edit():
+    now = datetime.datetime.now()
+    return render_template('person_edit.html', current_time=now.ctime())
+
