ea5677e9d74aa8c983114cb6c7c6ce5a612904a3
mhbeyatli <habibbeyatli@gmail.com>
2015-12-08 10:00:42 +0200

    Deleting unnecessary part

--- Medals.py
+++ Medals.py
@@ -163,29 +163,3 @@ def deletemedalrec(id):
 
             return redirect(url_for('medals_page'))
 
-def get_sqldb_dsn(vcap_services):
-    """Returns the data source name for ElephantSQL."""
-    parsed = json.loads(vcap_services)
-    uri = parsed["elephantsql"][0]["credentials"]["uri"]
-    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
-    user, password, host, _, port, dbname = match.groups()
-    dsn = """user='{}' password='{}' host='{}' port={}
-             dbname='{}'""".format(user, password, host, port, dbname)
-    return dsn
-
-if __name__ == '__main__':
-    app.secret_key = 'sZXCk15415523jdsIEOE98721'
-    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-    if VCAP_APP_PORT is not None:
-        port, debug = int(VCAP_APP_PORT), False
-    else:
-        port, debug = 5000, True
-
-    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
-    if VCAP_SERVICES is not None:
-        app.config['dsn'] = get_sqldb_dsn(VCAP_SERVICES)
-    else:
-        app.config['dsn'] = """dbname='xwacqlyq' host='jumbo.db.elephantsql.com' port=5432 user='xwacqlyq' password='eDso0SkacPcR_R6fDRmk0iISfAqh9xjN'"""
-
-    app.run(host='0.0.0.0', port=port, debug=debug)
-
